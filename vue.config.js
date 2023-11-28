31bdac9f7f0b1fb14afbdc1dad063579687b5022
Yulia <julia3694510@gmail.com>
2023-11-28 18:42:55 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/client-creation-form/' : '/',
   chainWebpack: config => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
